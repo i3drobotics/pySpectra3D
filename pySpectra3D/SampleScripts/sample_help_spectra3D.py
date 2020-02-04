d1b13f62ff0fd0d6d22a3649f2758d17023da59b
bknight-i3drobotics <bknight@i3drobotics.com>
2020-02-04 18:01:49 +0000

    update python sample help script

--- pySpectra3D/SampleScripts/sample_help_spectra3D.py
+++ pySpectra3D/SampleScripts/sample_help_spectra3D.py
@@ -1,9 +1,9 @@
 from Spectra3D import Spectra3D
 
-sp3d = Spectra3D()
+# View help on entire class
+help(Spectra3D)
 
-help(sp3d.read_ply)
-
-help(sp3d.read_spectra_labels)
-
-help(sp3d.read_spectra_list)
+# View help on individual functions
+help(Spectra3D.read_ply)
+help(Spectra3D.read_spectra_labels)
+help(Spectra3D.read_spectra_list)
